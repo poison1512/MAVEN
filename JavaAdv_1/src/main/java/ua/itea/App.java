c75565fc32d84d46996aae4804406756a8a35d23
Poison <artofpoison68@gmail.com>
2021-06-20 17:39:52 +0300

    +Jar file

---
+++ JavaAdv_1/src/main/java/ua/itea/App.java
@@ -0,0 +1,15 @@
+package ua.itea;
+
+import less02.PudgeLocalise2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	PudgeLocMav lPudbe = new PudgeLocMav();
+    }
+}
